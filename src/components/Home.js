da74fe23ff4036084e19c774465514dfd0cc3269
Cesar Herrera <cesarherr321@gmail.com>
2023-07-11 00:41:13 -0400

    Add Styles home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,25 @@
+import styles from '../styles/Home.module.css';
+
 function Home() {
+  const homeContainer = {
+    marginTop: '3rem',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  };
+
   return (
-    <div className="homeContainer">
+    <div className={styles.homeContainer} style={homeContainer}>
       <h3>Welcome to our page!</h3>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nobis
         officiis ratione blanditiis incidunt saepe, consequuntur vel deserunt
         molestiae dolorum delectus ex laboriosam amet ad perferendis, labore
-        facere porro dignissimos.
+        facere porro dignissimos. Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Fugit nobis officiis ratione blanditiis incidunt
+        saepe, consequuntur vel deserunt molestiae dolorum delectus ex
+        laboriosam amet ad perferendis, labore facere porro dignissimos.
       </p>
     </div>
   );
